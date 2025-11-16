6b4c9a68a3ee6a834b0ed3d4bdcb72933a136a06
= <ilyasseladi01@gamil.com>
2025-11-16 21:38:55 +0100

    modification

--- backend/fichier/src/main/java/app/fichier/Controller/AdminController.java
+++ backend/fichier/src/main/java/app/fichier/Controller/AdminController.java
@@ -10,28 +10,37 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@RequestMapping("/admin/")
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
     private final DemandeService service;
     @GetMapping("/demandes")
-    public Page<AdminDemande> getMethodName(@RequestParam(defaultValue ="0") int page,
+    public ResponseEntity<Page<AdminDemande>> getMethodName(@RequestParam(defaultValue ="0") int page,
     @RequestParam(defaultValue = "10") int size,
     @RequestParam(required = false) String status,
     @RequestParam(required = false) String type,
     @RequestParam(required = false) String nomCommune
     ) {
         Pageable pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "dateCreation"));
-        return service.listerDemande(pageRequest, status, nomCommune, type);
+        Page<AdminDemande> result = service.listerDemande(pageRequest, status, nomCommune, type);
+        return ResponseEntity.ok().body(result);
         
     }
+    @GetMapping("/details/{id}")
+    public ResponseEntity<AdminDemande> getMethodName(@PathVariable String id) {
+        var admin = service.getDetails(id);
+        return ResponseEntity.ok().body(admin);
+    }
+    
     
 
 }
